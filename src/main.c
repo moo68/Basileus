808ebe090c0ddd173756867c678aa8567f220642
moo68 <jack@ruzynski.com>
2026-07-17 14:24:34 -0500

    Fixed Mac support

--- src/main.c
+++ src/main.c
@@ -280,8 +280,14 @@ SDL_Window *create_window() {
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Initialized SDL3");
 
     // Set OpenGL version and profile
+#ifndef __APPLE__
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+#endif
+#ifdef __APPLE__
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+#endif
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
 
