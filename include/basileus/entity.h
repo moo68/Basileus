fc87c7305748f17df27642dc360a7857055b7dd8
moo68 <jack@ruzynski.com>
2026-07-20 22:36:33 -0500

    Transform entity

--- include/basileus/entity.h
+++ include/basileus/entity.h
@@ -3,15 +3,19 @@
 
 #include "basileus/transform.h"
 
+/* 
+ * This number should never be UINT32_MAX or any other maximum. Maximum is
+ * reserved as a sentinel value.
+ */
 #define MAX_ENTITIES (16)
 
 
 /*
  * The system of arrays used to keep track of entity data. This only deals with
- * entities and does nothing related to any kind of component. 
+ * entities and doesn't track components in any capacity. 
  */
 typedef struct {
-     /*
+    /*
      * This list contains the generation number for every possible entity ID. An
      * index of this array is itself an entity's ID. So for example, the value
      * contained at index 4 is the generation number of Entity 4.
@@ -37,12 +41,53 @@ typedef struct {
     uint32_t used_count;       
 } entity_tracker_t;
 
+/*
+ * The system of arrays that tracks all possible components used by all possible
+ * entities.
+ */
+typedef struct {
+    /*
+     * The sparse entity arrays include every entity via their indices (index 7
+     * directly corresponds to Entity 7, etc). The actual contents of each
+     * index is itself an index into the corresponding component array that
+     * contains the actual component data. UINT32_MAX is a sentinel value that
+     * means no entity contains the component that the array points to.
+     */
+    uint32_t sparse_transform_entities[MAX_ENTITIES];
+
+    /*
+     * The component arrays are dense arrays that contain actual component
+     * data. Component data can be either primitive types or handles to other,
+     * larger chunks of data. Components should always only ever store POD
+     * (plain old data)!
+     */
+    Transform transform_components[MAX_ENTITIES];
+    uint32_t transform_component_count;
+
+    /*
+     * The dense entity arrays directly mirror the component arrays of the
+     * same component type. For example, if a component in a component array
+     * at index 3 corresponds to Entity 12, then the value at index 3 in the
+     * dense entity array will also correspond to Entity 12. The values stored
+     * in the dense entity arrays are entity IDs. This allows easy traversal
+     * from one specific component to the entity that uses that component.
+     */
+    uint32_t dense_transform_entities[MAX_ENTITIES];
+} component_tracker_t;
+
 
 entity_tracker_t create_entity_tracker(void);
 
 uint32_t create_entity(entity_tracker_t *tracker);
 
 void destroy_entity(entity_tracker_t *tracker, uint32_t id);
+
+component_tracker_t create_component_tracker(void);
+
+void create_transform_component(component_tracker_t *tracker, uint32_t id,
+                                Transform transform);
+
+void destroy_transform_component(component_tracker_t *tracker, uint32_t id);
 
 #endif
 
--- src/entity.c
+++ src/entity.c
@@ -1,6 +1,9 @@
 #include "basileus/entity.h"
 
-#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "basileus/transform.h"
 
 
 entity_tracker_t create_entity_tracker(void) {
@@ -20,9 +23,62 @@ uint32_t create_entity(entity_tracker_t *tracker) {
     return entity_id;
 }
 
+// TODO: Actually deal with the entity's components
 void destroy_entity(entity_tracker_t *tracker, uint32_t id) {
     tracker->generations[id]++;
     tracker->used_count--;
     tracker->free_stack[tracker->used_count] = id;
+}
+
+component_tracker_t create_component_tracker(void) {
+    component_tracker_t tracker = {0};
+
+    memset(tracker.sparse_transform_entities, UINT32_MAX,
+           sizeof(tracker.sparse_transform_entities));
+    memset(tracker.dense_transform_entities, UINT32_MAX,
+           sizeof(tracker.dense_transform_entities));
+
+    return tracker;
+}
+
+void create_transform_component(component_tracker_t *tracker, uint32_t id,
+                                Transform transform) {
+    uint32_t component_index = tracker->transform_component_count;
+
+    tracker->sparse_transform_entities[id] = component_index;
+
+    tracker->transform_components[component_index] = transform;
+    tracker->transform_component_count++;
+
+    tracker->dense_transform_entities[component_index] = id;
+}
+
+void destroy_transform_component(component_tracker_t *tracker, uint32_t id) {
+    uint32_t component_index = tracker->sparse_transform_entities[id];
+    uint32_t last_component_index = tracker->transform_component_count - 1;
+
+    Transform *last_component = &tracker->transform_components[last_component_index];
+
+    // Delete component data, keeping the array dense
+    if (component_index != last_component_index) {
+        memcpy(&tracker->transform_components[component_index], last_component,
+               sizeof(Transform));
+    }
+
+    memset(last_component, 0, sizeof(Transform));
+    tracker->transform_component_count--;
+
+    // Delete entity ID reference, keeping the array dense
+    if (component_index != last_component_index) {
+        memcpy(&tracker->dense_transform_entities[component_index],
+               &tracker->dense_transform_entities[last_component_index],
+               sizeof(uint32_t));
+    }
+
+    memset(&tracker->dense_transform_entities[last_component_index], UINT32_MAX,
+           sizeof(uint32_t));
+
+    // Delete sparse entity ID
+    memset(&tracker->sparse_transform_entities[id], UINT32_MAX, sizeof(uint32_t));
 }
 
